4d7366728a16d90fe36d7f9c1279e3b88a859270
TylerMcK <tiga@hotmail.com.au>
2016-08-21 16:45:30 +1000

    Cleaned up code, added level increasing and changed sprites.

--- pong_assessment.c
+++ pong_assessment.c
@@ -10,8 +10,8 @@
 // Configuration
 #define DELAY (10) /* Millisecond delay between game updates */
 
-#define HERO_WIDTH (5)
-#define HERO_HEIGHT (5)
+#define PLAYER_WIDTH (1)
+#define PLAYER_HEIGHT (5)
 #define ZOMBIE_WIDTH (5)
 #define ZOMBIE_HEIGHT (5)
 
@@ -23,163 +23,155 @@ bool update_screen = true; /* Set to false to prevent screen update. */
 int lives  = 10;
 int score = 0;
 int level = 1;
-int time = 0;
-
-static char * hero_image =
-/**/	"H   H"
-/**/	"H   H"
-/**/	"HHHHH"
-/**/	"H   H"
-/**/	"H   H";
-
-static char * zombie_image =
-/**/	"ZZZZZ"
-/**/	"   Z "
-/**/	"  Z  "
-/**/	" Z   "
-/**/	"ZZZZZ";
+int clock = 0;
+bool clock_pause = false;
 
 static char * msg_image =
 /**/	"Goodbye and thank-you for playing ZombieDash Jr."
 /**/	"            Press any key to exit...            ";
 
-// (c) Declare a sprite_id called hero.
-sprite_id hero;
+// Declare sprite_ids
+sprite_id player;
 
-// (m) Declare a sprite_id called zombie.
-sprite_id zombie;
 
-// Setup game.
+// Game setup
 void setup(void) {
-	// Useful variables.
-	int w = screen_width(), wh = HERO_WIDTH, wz = ZOMBIE_WIDTH;
-	int h = screen_height(), hh = HERO_HEIGHT, hz = ZOMBIE_HEIGHT;
+	// Variable Declaration
+	int sw = screen_width(), pw = PLAYER_WIDTH;
+	int sh = screen_height(), ph = PLAYER_HEIGHT;
 
-	// Keep the next line intact.
-	show_screen();
-
-	// (d) Set up the hero at the centre of the screen.
-	hero = sprite_create( (w-wh)/2, (h-hh)/2, wh, hh, hero_image );
-
-	// (e) Draw the hero.
-	sprite_draw( hero );
+	// --- Create and Draw Sprites --- //
+	// Player Entity
+	player = sprite_create(3, (sh-ph)/2, pw, ph, "|");
+	sprite_draw(player);
 	
-	// Keep the next line intact.
-	show_screen();
-
 	// (n)	Set up the zombie at a random location on the screen.
-	int xrange = w - wz - 2;
-	int yrange = h - hz - 2;
-	srand( get_current_time() );
-	int zx = rand() % xrange + 1;
-	int zy = rand() % yrange + 1;
-	zombie = sprite_create( zx, zy, wz, hz, zombie_image );
+	//int xrange = w - wz - 2;
+	//int yrange = h - hz - 2;
+	//srand( get_current_time() );
+	//int zx = rand() % xrange + 1;
+	//int zy = rand() % yrange + 1;
+	//zombie = sprite_create( zx, zy, wz, hz, zombie_image );
 
 	// (o) Draw the zombie.
-	sprite_draw( zombie );
+	//sprite_draw( zombie );
 
 	// (p) Set the zombie in motion.
-	sprite_turn_to ( zombie, 0.2, 0 );
-	sprite_turn( zombie, rand() % 360 );
+	//sprite_turn_to ( zombie, 0.2, 0 );
+	//sprite_turn( zombie, rand() % 360 );
 
-	// Keep the next line intact.
+	// Refresh the screen
 	show_screen();
 }
 
 // Play one turn of game.
 void process(void) {
-	int w = screen_width(), wh = HERO_WIDTH, wz = ZOMBIE_WIDTH;
-	int h = screen_height(), hh = HERO_HEIGHT, hz = ZOMBIE_HEIGHT;
+	int sw = screen_width(), pw = PLAYER_WIDTH;
+	int sh = screen_height(), ph = PLAYER_HEIGHT;
+	int clock = 0;
 
-	// (f) Get a character code from standard input without waiting.
+	// Check player input
 	int key = get_char();
 	
 	// (y) Test for end of game.
 
 	// (g)	Get the current screen coordinates of the hero in integer variables 
 	//		by rounding the actual coordinates.
-	int hx = sprite_x( hero );
-	int hy = sprite_y( hero );
+	int px = sprite_x(player);
+	int py = sprite_y(player);
 
-	// (h) Move hero left according to specification.
-	if ( key == 'a' && hx > 1 ) {
-		sprite_move( hero, -1, 0 );
+	// --- Key Inputs --- //
+	// -- Player Controls -- //
+	// Move player Up
+	if ( key == 'w' && px > 1 ) {
+		sprite_move( player, 0, -1 );
+	}
+	// Move player down
+	else if ( key == 's' && px < sw - pw - 1) {
+		sprite_move( player, 0, +1 );
+	}
+	// -- System Controls -- //
+	// Level change
+	else if (key == 'l') {
+		if (level == 4) {
+			level = 1;
+		} else {
+		       level++;
+		}
 	}
 
-	// (i) Move hero right according to specification.
-	else if ( key == 'd' && hx < w - wh - 1) {
-		sprite_move( hero, +1, 0 );
-	}
-
-	// (j) Move hero up according to specification.
-
-	// (k) Move hero down according to specification.
+	if (!clock_pause) {
+		clock++;
+	}	
 
 	// (q.a) Test to move the zombie if key is 'z' or ERROR.
-	else if ( key == 'z' || key < 0 ) {
+	//else if ( key == 'z' || key < 0 ) {
 		// (r) Zombie takes one step.
-		sprite_step( zombie );
+	//	sprite_step( zombie );
 
 		// (s) Get screen location of zombie.
-		int zx = round( sprite_x( zombie ) );
-		int zy = round( sprite_y( zombie ) );
+	//	int zx = round( sprite_x( zombie ) );
+	//	int zy = round( sprite_y( zombie ) );
 
 		// (t) Get the displacement vector of the zombie.
-		double zdx = sprite_dx( zombie );
-		double zdy = sprite_dy( zombie );
-		bool dir_changed = false;
+	//	double zdx = sprite_dx( zombie );
+	//	double zdy = sprite_dy( zombie );
+	//	bool dir_changed = false;
 
 		// (u) Test to see if the zombie hit the left or right border.
-		if ( zx == 0 || zx == w - 5 ) {
-			zdx = -zdx;
-			dir_changed = true;
-		}
+	//	if ( zx == 0 || zx == w - 5 ) {
+	//		zdx = -zdx;
+	//		dir_changed = true;
+	//	}
 
 		// (v) Test to see if the zombie hit the top or bottom border.
-		if ( zy == 6  || zy == h - 5 ) {
-			zdy = -zdy;
-			dir_changed = true;
-		}
+	//	if ( zy == 6  || zy == h - 5 ) {
+	//		zdy = -zdy;
+	//		dir_changed = true;
+	//	}
 
 		// (w) Test to see if the zombie needs to step back and change direction.
-		if ( dir_changed ) {
-			sprite_back( zombie );
-			sprite_turn_to( zombie, zdx, zdy );
-		}
+	//	if ( dir_changed ) {
+	//		sprite_back( zombie );
+	//		sprite_turn_to( zombie, zdx, zdy );
+	//	}
 
 	// (q.b) End else-if test to move the zombie if key is 'z' or negative.
-	}
+	//}
 	
 	// Leave next line intact
 	clear_screen();
 
-	// --- Inner Game Border --- //
+	// --- Game Border --- //
 	// Upper Top Border
-	draw_line(0, 0, w - 1, 0, '*');
+	draw_line(0, 0, sw - 1, 0, '*');
 	// Inner Top Border
-	draw_line(0, 6, w - 1, 6, '*');
+	draw_line(0, 6, sw - 1, 6, '*');
 	//Bottom Border
-	draw_line(0, h-1, w - 1, h-1, '*');
+	draw_line(0, sh-1, sw - 1, sh-1, '*');
 	//Left Border
-	draw_line(0, 0, 0, h-1, '*');
+	draw_line(0, 0, 0, sh-1, '*');
 	//Right Border
-	draw_line(w-1, 0, w - 1, h-1, '*');
+	draw_line(sw-1, 0, sw - 1, sh-1, '*');
 
 	// --- Info Panel --- //
-	draw_formatted(w/4, 3, "Lives = %2d *Score = %2d *Level = %2d *Time = %2d",
-		       	lives, score, level, time);
+	draw_formatted(sw/4, 3, "Lives = %2d *Score = %2d *Level = %2d *Time = %2d",
+		       	lives, score, level, clock);
 
 	// (l)	Draw the hero.
-	sprite_draw( hero );
+	sprite_draw(player);
 
 	// (x)	Draw the zombie.
-	sprite_draw( zombie );
+	//sprite_draw( zombie );
 }
 
 // Clean up game
 void cleanup(void) {
 	// STATEMENTS
 }
+
+// Help screen
+
 
 // Program entry point.
 int main(void) {
