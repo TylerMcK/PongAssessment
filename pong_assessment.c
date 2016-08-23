a822dc503716848c200c0e848433a1fc12c1b728
TylerMcK <tiga@hotmail.com.au>
2016-08-23 21:47:57 +1000

    Added help and timer + pause

--- pong_assessment.c
+++ pong_assessment.c
@@ -5,16 +5,22 @@
 #include <cab202_sprites.h>
 #include <cab202_timers.h>
 
-// ADD INCLUDES HERE
-
 // Configuration
 #define DELAY (10) /* Millisecond delay between game updates */
 
 #define PLAYER_WIDTH (1)
-#define PLAYER_HEIGHT (5)
 #define ZOMBIE_WIDTH (5)
 #define ZOMBIE_HEIGHT (5)
 
+//FINISH
+int PaddleHeight() {
+	int height = 7;
+	if (screen_height() < 21) {
+		size = (screen_height() - 5 - 1) / 2;
+	}
+}	
+
+#define PLAYER_HEIGHT (5)
 // Game state.
 bool game_over = false; /* Set this to true when game is over */
 bool update_screen = true; /* Set to false to prevent screen update. */
@@ -23,8 +29,26 @@ bool update_screen = true; /* Set to false to prevent screen update. */
 int lives  = 10;
 int score = 0;
 int level = 1;
-int clock = 0;
-bool clock_pause = false;
+int startingTime;
+int currentTime;
+int realTime = 0;
+int pauseTime = 0;
+
+bool initial = true;
+
+int sw;
+int sh; 
+int pw;
+int ph;
+	
+static char * player =
+/**/	"|"
+/**/	"|"
+/**/	"|"
+/**/	"|"
+/**/	"|"
+/**/	"|"
+/**/	"|"
 
 static char * msg_image =
 /**/	"Goodbye and thank-you for playing ZombieDash Jr."
@@ -33,16 +57,42 @@ static char * msg_image =
 // Declare sprite_ids
 sprite_id player;
 
+// Environment Setup
+void EnvironmentSetup(void) {
+	clear_screen();
+	sw = screen_width();
+	sh = screen_height();
+	// --- Game Border --- //
+	// Upper Top Border
+	draw_line(0, 0, sw - 1, 0, '*');
+	// Inner Top Border
+	draw_line(0, 6, sw - 1, 6, '*');
+	//Bottom Border
+	draw_line(0, sh-1, sw - 1, sh-1, '*');
+	//Left Border
+	draw_line(0, 0, 0, sh-1, '*');
+	//Right Border
+	draw_line(sw-1, 0, sw - 1, sh-1, '*');
+
+	// --- Info Panel --- //
+	draw_formatted(sw/4, 3, "Lives = %2d *Score = %2d *Level = %2d *Time = %2d",
+		       	lives, score, level, realTime);
+
+	show_screen();
+
+}
+
+
 
 // Game setup
 void setup(void) {
 	// Variable Declaration
-	int sw = screen_width(), pw = PLAYER_WIDTH;
-	int sh = screen_height(), ph = PLAYER_HEIGHT;
-
+	startingTime = get_current_time();
+	int pw = PLAYER_WIDTH, ph = PLAYER_HEIGHT;
 	// --- Create and Draw Sprites --- //
 	// Player Entity
-	player = sprite_create(3, (sh-ph)/2, pw, ph, "|");
+	player = sprite_create(3, (sh-ph)/2, pw, ph, "|");	
+
 	sprite_draw(player);
 	
 	// (n)	Set up the zombie at a random location on the screen.
@@ -64,16 +114,28 @@ void setup(void) {
 	show_screen();
 }
 
+// Help screen
+void HelpScreen(void) {
+	clear_screen();
+	draw_string(30, 10, "Tyler McKerihan N9447482");
+	draw_string(20, 12, "Instructions:");
+	draw_string(20, 13, "Move up: w");
+	draw_string(20, 14, "Press Any Key to Continue");
+	show_screen();
+	getchar();
+	clear_screen();
+	return;
+}
+
 // Play one turn of game.
 void process(void) {
-	int sw = screen_width(), pw = PLAYER_WIDTH;
-	int sh = screen_height(), ph = PLAYER_HEIGHT;
-	int clock = 0;
+	currentTime = get_current_time() - startingTime;
+	realTime = currentTime - pauseTime;
 
 	// Check player input
 	int key = get_char();
-	
-	// (y) Test for end of game.
+
+	EnvironmentSetup();
 
 	// (g)	Get the current screen coordinates of the hero in integer variables 
 	//		by rounding the actual coordinates.
@@ -99,10 +161,14 @@ void process(void) {
 		       level++;
 		}
 	}
-
-	if (!clock_pause) {
-		clock++;
-	}	
+	// Help Screen
+	if (key == 'h' || initial) {
+		int tempTime = get_current_time();
+		HelpScreen();
+		initial = false;
+		EnvironmentSetup();
+		pauseTime += get_current_time() - tempTime;
+	}
 
 	// (q.a) Test to move the zombie if key is 'z' or ERROR.
 	//else if ( key == 'z' || key < 0 ) {
@@ -139,25 +205,6 @@ void process(void) {
 	// (q.b) End else-if test to move the zombie if key is 'z' or negative.
 	//}
 	
-	// Leave next line intact
-	clear_screen();
-
-	// --- Game Border --- //
-	// Upper Top Border
-	draw_line(0, 0, sw - 1, 0, '*');
-	// Inner Top Border
-	draw_line(0, 6, sw - 1, 6, '*');
-	//Bottom Border
-	draw_line(0, sh-1, sw - 1, sh-1, '*');
-	//Left Border
-	draw_line(0, 0, 0, sh-1, '*');
-	//Right Border
-	draw_line(sw-1, 0, sw - 1, sh-1, '*');
-
-	// --- Info Panel --- //
-	draw_formatted(sw/4, 3, "Lives = %2d *Score = %2d *Level = %2d *Time = %2d",
-		       	lives, score, level, clock);
-
 	// (l)	Draw the hero.
 	sprite_draw(player);
 
@@ -169,8 +216,6 @@ void process(void) {
 void cleanup(void) {
 	// STATEMENTS
 }
-
-// Help screen
 
 
 // Program entry point.
